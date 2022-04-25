0223e679267ad2d977cb546517d6e98e3be353f4
Kenny Clare <kmitchellclare@gmail.com>
2022-04-25 13:37:05 -0700

    commit tree 9cd8ceb7df3f6c1522c194392a23324de86096eb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bath my dog')
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(special = '*') {
+    return function (adjective = "a good student") {
+        return `You are ${special}${adjective}${special}`
+    }
+}
+
+function wrapAdjective(special = '||') {
+    return function (adjective = "a dedicated programmer") {
+        return `You are ${special}${adjective}${special}!`
+    }
+}
